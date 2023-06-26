0dc0d891fe3ccb75b48c73128d2793418a037297
Chathurika Gajanayaka <chathurikagajanayaka@chathurikas-air.home>
2023-06-26 22:49:02 +0200

    Add multiplication game code

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -35,7 +35,10 @@ function runGame(gameType) {
   if(gameType === "addition") {
     displayAdditionQuestion(num1 , num2);
 
-  } else{
+  } else if(gameType === "multiply"){
+    displayMultiplyQuestion(num1 , num2);
+  }
+  else{
     alert(`Unknown game type: ${gameType}`);
     throw `Unknown game type: ${gameType}.Aborting!`;
   }
@@ -78,7 +81,11 @@ let operator =  document.getElementById("operator").innerText;
     
 if(operator === "+"){
   return[opperand1 + opperand2, "addition"];
-}else{
+} else if(operator === "x"){
+  return[opperand1 * opperand2, "multiply"];
+}
+
+else{
   alert(`Unimplemented operator ${operator}`);
   throw `unimplemented operator ${operator}.Aborting!`;
 }
@@ -90,7 +97,6 @@ if(operator === "+"){
 /**
  * Gets the current score from the DOM and increments it by 1
  */
-
 
 function incrementScore(){
   let oldScore = parseInt(document.getElementById("score").innerText);
@@ -120,7 +126,10 @@ function displaySubtractQuestion(){
 
 }
 
-function displayMultiplyQuestion(){
+function displayMultiplyQuestion(operand1 , operand2){
+  document.getElementById('operand1').textContent = operand1;
+  document.getElementById('operand2').textContent = operand2;
+  document.getElementById('operator').textContent = "x";
 
 
 }
